6593ea867491a977d1da87f4e586221edc2bd9c8
DENOBODY2 <amschildkraut@gmail.com>
2023-10-12 23:13:37 -0500

    -resource pack thing scrapped

--- src/main/java/net/denobody2/loungingdonuts/LoungingDonuts.java
+++ src/main/java/net/denobody2/loungingdonuts/LoungingDonuts.java
@@ -4,16 +4,29 @@ import com.mojang.logging.LogUtils;
 import net.denobody2.loungingdonuts.registry.ModBlocks;
 import net.denobody2.loungingdonuts.registry.ModCreativeTabs;
 import net.denobody2.loungingdonuts.registry.ModItems;
+import net.minecraft.SharedConstants;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.packs.PackType;
+import net.minecraft.server.packs.PathPackResources;
+import net.minecraft.server.packs.metadata.pack.PackMetadataSection;
+import net.minecraft.server.packs.repository.Pack;
+import net.minecraft.server.packs.repository.PackSource;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.AddPackFindersEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.resource.DelegatingPackResources;
 import org.slf4j.Logger;
+
+import java.nio.file.Path;
+import java.util.List;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(LoungingDonuts.MOD_ID)
@@ -35,6 +48,7 @@ public class LoungingDonuts
         ModItems.ITEMS.register(modEventBus);
         ModCreativeTabs.CREATIVE_MODE_TABS.register(modEventBus);
         ModBlocks.BLOCKS.register(modEventBus);
+        modEventBus.addListener(this::packSetup);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
@@ -46,6 +60,39 @@ public class LoungingDonuts
     {
         // Some common setup code
 
+    }
+    public void packSetup(AddPackFindersEvent event) {
+        // Resource Packs
+        //this.setupRealisticPack(event);
+    }
+    private void setupRealisticPack(AddPackFindersEvent event) {
+        if (event.getPackType() == PackType.CLIENT_RESOURCES) {
+            Path resourcePath = ModList.get().getModFileById(LoungingDonuts.MOD_ID).getFile().findResource("packs/realistic_donuts");
+            PathPackResources pack = new PathPackResources(ModList.get().getModFileById(LoungingDonuts.MOD_ID).getFile().getFileName() + ":" + resourcePath, resourcePath, false);
+            System.out.println("System.out.println READ THIS DENOBODY2!" + pack);
+            Path baseResourcePath = ModList.get().getModFileById(LoungingDonuts.MOD_ID).getFile().findResource("packs/realistic_donuts");
+            PathPackResources basePack = new PathPackResources(ModList.get().getModFileById(LoungingDonuts.MOD_ID).getFile().getFileName() + ":" + baseResourcePath, baseResourcePath, false);
+            String name = "builtin/realistic_donuts";
+            String title = "pack.loungingdonuts.realistic_donuts.title";
+            String description = "pack.loungingdonuts.realistic_donuts.description";
+            List<PathPackResources> mergedPacks = List.of(pack,basePack);
+            Pack.ResourcesSupplier resourcesSupplier =(string) -> new DelegatingPackResources(name, true, new PackMetadataSection(Component.translatable(description), SharedConstants.getCurrentVersion().getPackVersion(PackType.CLIENT_RESOURCES)), mergedPacks);
+            Pack.Info info = Pack.readPackInfo(name, resourcesSupplier);
+            if (info != null) {
+                event.addRepositorySource((source) ->
+                        source.accept(Pack.create(
+                                name,
+                                Component.translatable(title),
+                                false,
+                                resourcesSupplier,
+                                info,
+                                PackType.CLIENT_RESOURCES,
+                                Pack.Position.TOP,
+                                false,
+                                PackSource.BUILT_IN)
+                        ));
+            }
+        }
     }
 
     // Add the example block item to the building blocks tab
